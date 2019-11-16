0d521741b997ca966ca3ee91dcbc167177ba49c3
Carson Keller <carsonjkeller@gmail.com>
2019-11-16 13:10:49 -0700

    Sqlite

---
+++ EMRstar/EMRstar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EMRstar
+//
+//  Created by Carson Keller on 11/16/19.
+//  Copyright © 2019 Carson Keller. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EMRstar/EMRstar/SQLite.swift
@@ -0,0 +1,9 @@
+//
+//  SQLite.swift
+//  EMRstar
+//
+//  Created by Carson Keller on 11/16/19.
+//  Copyright © 2019 Carson Keller. All rights reserved.
+//
+
+import Foundation
